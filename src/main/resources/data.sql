c68144c579b4b5bcb10207fdbc36e2538e9e8abe
vishnusj <vishnusj1992@gmail.com>
2020-05-13 16:56:30 -0600

    Initial Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into ALIEN values (100, 'Vishnu', 'Java');
+insert into alien values (101, 'User', 'C++');
+insert into alien values (102, 'UserAgain', 'Java');
+insert into alien values (103, 'AA', 'Java');
